ec75613b7af974ff98b1d4716a5923040cbe8bc0
Maurício Grigoleto <102479476+mgrigoleto@users.noreply.github.com>
2022-04-29 17:00:42 -0300

    Finished question 1

--- PSET2/Script.sql
+++ PSET2/Script.sql
@@ -1,2 +1,3 @@
 /* QUESTÃO 01 */
-SELECT AVG(salario) FROM funcionarios GROUP BY numero_departamento;
+SELECT numero_departamento as Número_do_Departamento, CAST(AVG(salario) AS DECIMAL(10,2)) AS Média_salarial FROM funcionarios GROUP BY numero_departamento;
+
