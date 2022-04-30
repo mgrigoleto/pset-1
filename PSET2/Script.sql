d203c4b57cb07ed317263031894341aaf404aaeb
Maurício Grigoleto <102479476+mgrigoleto@users.noreply.github.com>
2022-04-30 13:38:51 -0300

    Solved 11st question and updated some formats

--- PSET2/Script.sql
+++ PSET2/Script.sql
@@ -1,38 +1,44 @@
 /* QUESTÃO 01 */
-SELECT numero_departamento AS Número_do_Departamento, CAST(AVG(salario) AS DECIMAL(10,2)) AS Média_salarial FROM funcionarios GROUP BY numero_departamento;
+SELECT nome_departamento AS Departamento, CONCAT('R$ ', CAST(AVG(salario) AS DECIMAL(10,2))) AS Média_salarial
+FROM funcionarios f INNER JOIN departamento d
+WHERE f.numero_departamento = d.numero_departamento
+GROUP BY nome_departamento;
 
 /* QUESTÃO 02 */
-SELECT sexo AS Sexo, CAST(AVG(salario) AS DECIMAL(10,2)) AS Média_salarial FROM funcionarios GROUP BY Sexo;
+SELECT CASE WHEN sexo = 'M' THEN 'Masculino' WHEN sexo = 'm' THEN 'Masculino'
+WHEN sexo = 'F' THEN 'Feminino' WHEN sexo = 'f' THEN 'Feminino' END AS Sexo,
+CONCAT('R$ ', CAST(AVG(salario) AS DECIMAL(10,2))) AS Média_salarial FROM funcionarios GROUP BY Sexo;
 
 /* QUESTÃO 03 */
 /* Floor aproxima o resultado da data atual MENOS a data de nascimento. Como o resultado será em dias, basta dividir por 365.25,
 que é a quantidade de dias que tem em um ano. */
-SELECT nome_departamento AS Departamento, primeiro_nome AS Nome, nome_meio AS Nome_do_meio, ultimo_nome AS Sobrenome, 
+SELECT nome_departamento AS Departamento, CONCAT(f.primeiro_nome, ' ', f.nome_meio, ' ', f.ultimo_nome) AS Nome_completo, 
 data_nascimento AS Data_de_nascimento, 
 FLOOR(DATEDIFF(CURDATE(), data_nascimento)/365.25) AS Idade, 
-salario AS Salário 
+CONCAT('R$ ', CAST((salario) AS DECIMAL(10,2))) AS Salário 
 FROM funcionarios f INNER JOIN departamento d
 WHERE f.numero_departamento = d.numero_departamento
 ORDER BY nome_departamento;
 
 /* QUESTÃO 04 */
-SELECT primeiro_nome AS Nome, nome_meio AS Nome_do_meio, ultimo_nome AS Sobrenome, FLOOR(DATEDIFF(CURDATE(), data_nascimento)/365.25) AS Idade, 
-salario AS Salário, CAST((salario*1.2) AS DECIMAL(10,2)) AS Novo_salário FROM funcionarios
+SELECT CONCAT(f.primeiro_nome, ' ', f.nome_meio, ' ', f.ultimo_nome) AS Nome_completo, FLOOR(DATEDIFF(CURDATE(), data_nascimento)/365.25) AS Idade, 
+CONCAT('R$ ', CAST((salario) AS DECIMAL(10,2))) AS Salário, CONCAT('R$ ', CAST((salario*1.2) AS DECIMAL(10,2))) AS Novo_salário FROM funcionarios f
 WHERE salario < '35000'
 UNION
-SELECT primeiro_nome AS Nome, nome_meio AS Nome_do_meio, ultimo_nome AS Sobrenome, FLOOR(DATEDIFF(CURDATE(), data_nascimento)/365.25) AS Idade, 
-salario AS Salário, CAST((salario*1.15) AS DECIMAL(10,2)) AS Novo_salário FROM funcionarios
+SELECT CONCAT(f.primeiro_nome, ' ', f.nome_meio, ' ', f.ultimo_nome) AS Nome_completo, FLOOR(DATEDIFF(CURDATE(), data_nascimento)/365.25) AS Idade, 
+CONCAT('R$ ', CAST((salario) AS DECIMAL(10,2))) AS Salário, CONCAT('R$ ', CAST((salario*1.15) AS DECIMAL(10,2))) AS Novo_salário FROM funcionarios f
 WHERE salario >= '35000';
 
 /* QUESTÃO 05 */
-SELECT nome_departamento AS Departamento, g.primeiro_nome AS Gerente, f.primeiro_nome AS Funcionário, salario AS Salários
+SELECT nome_departamento AS Departamento, g.primeiro_nome AS Gerente, f.primeiro_nome AS Funcionário, 
+CONCAT('R$ ', CAST((salario) AS DECIMAL(10,2))) AS Salário
 FROM departamento d INNER JOIN funcionarios f, 
 (SELECT primeiro_nome, cpf FROM funcionarios f INNER JOIN departamento d WHERE f.cpf = d.cpf_gerente) AS g
 WHERE d.numero_departamento = f.numero_departamento AND g.cpf = d.cpf_gerente
 ORDER BY d.nome_departamento ASC, f.salario DESC;
 
 /* QUESTÃO 06 */
-SELECT f.primeiro_nome AS Nome, f.nome_meio AS Nome_meio, f.ultimo_nome AS Sobrenome, dto.nome_departamento AS Departamento,
+SELECT CONCAT(f.primeiro_nome, ' ', f.nome_meio, ' ', f.ultimo_nome) AS Nome_completo, dto.nome_departamento AS Departamento,
 dpd.nome_dependente AS Dependente, FLOOR(DATEDIFF(CURDATE(), dpd.data_nascimento)/365.25) AS Idade_dependente,
 CASE WHEN dpd.sexo = 'M' THEN 'Masculino' WHEN dpd.sexo = 'm' THEN 'Masculino'
 WHEN dpd.sexo = 'F' THEN 'Feminino' WHEN dpd.sexo = 'f' THEN 'Feminino' END AS Sexo_dependente
@@ -41,8 +47,8 @@ INNER JOIN departamento dto ON f.numero_departamento = dto.numero_departamento
 INNER JOIN dependente dpd ON dpd.cpf_funcionario = f.cpf;
 
 /* QUESTÃO 07 */
-SELECT DISTINCT f.primeiro_nome AS Nome, f.nome_meio AS Nome_meio, f.ultimo_nome AS Sobrenome, dto.nome_departamento AS Departamento,
-f.salario AS Salário FROM funcionarios f
+SELECT DISTINCT CONCAT(f.primeiro_nome, ' ', f.nome_meio, ' ', f.ultimo_nome) AS Nome_completo, dto.nome_departamento AS Departamento,
+CONCAT('R$ ', CAST((f.salario) AS DECIMAL(10,2))) AS Salário FROM funcionarios f
 INNER JOIN departamento dto
 INNER JOIN dependente dpd
 WHERE dto.numero_departamento = f.numero_departamento AND
@@ -50,7 +56,7 @@ f.cpf NOT IN (SELECT dpd.cpf_funcionario FROM dependente dpd);
 
 /* QUESTÃO 08 */
 SELECT d.nome_departamento AS Departamento, p.nome_projeto AS Projeto,
-f.primeiro_nome AS Nome, f.nome_meio AS Nome_meio, f.ultimo_nome AS Sobrenome, t.horas AS Horas
+CONCAT(f.primeiro_nome, ' ', f.nome_meio, ' ', f.ultimo_nome) AS Nome_completo, t.horas AS Horas
 FROM funcionarios f INNER JOIN departamento d INNER JOIN projeto p INNER JOIN trabalha_em t
 WHERE d.numero_departamento = f.numero_departamento AND
 p.numero_projeto = t.numero_projeto AND
@@ -65,7 +71,14 @@ p.numero_projeto = t.numero_projeto
 GROUP BY p.Nome_projeto;
 
 /* QUESTÃO 10 */
-SELECT d.nome_departamento AS Departamento, CAST(AVG(f.salario) AS DECIMAL(10,2)) AS Média_salarial
+SELECT d.nome_departamento AS Departamento, CONCAT('R$ ', CAST(AVG(f.salario) AS DECIMAL(10,2))) AS Média_salarial
 FROM departamento d INNER JOIN funcionarios f
 WHERE d.numero_departamento = f.numero_departamento
 GROUP BY d.nome_departamento;
+
+/* QUESTÃO 11 */
+SELECT CONCAT(f.primeiro_nome, ' ', f.nome_meio, ' ', f.ultimo_nome) AS Nome_completo, p.nome_projeto AS Projeto,
+CONCAT('R$ ', CAST((t.horas*50) AS DECIMAL (10,2))) AS Recebimento
+FROM funcionarios f INNER JOIN projeto p INNER JOIN trabalha_em t
+WHERE f.cpf = t.cpf_funcionario AND p.numero_projeto = t.numero_projeto
+GROUP BY f.primeiro_nome;
